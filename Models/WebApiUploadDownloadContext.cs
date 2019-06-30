5a5b9fb7e4808f0afc8d6c29a88fc5afd8ef17cf
jrjosecarlos <52382277+jrjosecarlos@users.noreply.github.com>
2019-06-30 16:03:42 -0300

    Adicionado context a ser usado pela aplicação, assim como a string de conexão ao banco. Além disso, adicionado método ensureDatabase, para garantir o estado inicial do banco enquanto o projeto está no estado inicial.

---
+++ Models/WebApiUploadDownloadContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiUploadDownload.Models
+{
+    public class WebApiUploadDownloadContext : DbContext
+    {
+        public WebApiUploadDownloadContext(DbContextOptions<WebApiUploadDownloadContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebApiUploadDownload.Models;
 
 namespace WebApiUploadDownload
 {
@@ -14,7 +16,26 @@ namespace WebApiUploadDownload
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<WebApiUploadDownloadContext>();
+                    //TODO: remover quando as migrações forem adicionadas
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Erro ocorrido na criação do DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
