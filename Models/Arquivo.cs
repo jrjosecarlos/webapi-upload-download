e9c7ca2223f7740a92bf5aba9f9ed43efd6c0e9e
jrjosecarlos <52382277+jrjosecarlos@users.noreply.github.com>
2019-06-30 23:16:06 -0300

    Alterado relacionamento das classes de modelo, para comporem um conjunto de tabelas 0-para-1.

--- Models/Arquivo.cs
+++ Models/Arquivo.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApiUploadDownload.Models
 {
-    public abstract class Arquivo
+    public class Arquivo
     {
         public int ID { get; set; }
 
@@ -12,8 +12,13 @@ namespace WebApiUploadDownload.Models
         [StringLength(80)]
         public string Nome { get; set; }
 
+        [StringLength(200)]
+        public string Caminho { get; set; }
+
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         [Display(Name = "Data de Criação")]
         public DateTime DataCriacao { get; set; }
+
+        public ArquivoDB ArquivoDB { get; set; }
     }
 }
