9c63686e5f5cda148c717059222502c64379dd8c
jrjosecarlos <52382277+jrjosecarlos@users.noreply.github.com>
2019-06-30 22:45:20 -0300

    Alteradas opções do Startup para ignorar a serialização cíclica (como em relacionamentos entre entidades) de JSON. A opção padrão interrompe a serialização quando detecta um ciclo, gerando um JSON inválido.

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,12 @@ namespace WebApiUploadDownload
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
 
             services.AddDbContext<WebApiUploadDownloadContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("WebApiUploadDownloadContext")));
