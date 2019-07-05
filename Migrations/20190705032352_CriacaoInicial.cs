fb232c707d439c7b20043c1036c3b61ed4a3232a
jrjosecarlos <52382277+jrjosecarlos@users.noreply.github.com>
2019-07-05 00:33:41 -0300

    Removidas referências ao campo Caminho, cuja ideia foi embutida no novo campo NomeAraquivo.

---
+++ Migrations/20190705032352_CriacaoInicial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiUploadDownload.Migrations
+{
+    public partial class CriacaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Arquivos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 80, nullable: false),
+                    NomeReal = table.Column<string>(maxLength: 200, nullable: true),
+                    DataCriacao = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Arquivos", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArquivoDB",
+                columns: table => new
+                {
+                    ArquivoID = table.Column<int>(nullable: false),
+                    Conteudo = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArquivoDB", x => x.ArquivoID);
+                    table.ForeignKey(
+                        name: "FK_ArquivoDB_Arquivos_ArquivoID",
+                        column: x => x.ArquivoID,
+                        principalTable: "Arquivos",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArquivoDB");
+
+            migrationBuilder.DropTable(
+                name: "Arquivos");
+        }
+    }
+}
