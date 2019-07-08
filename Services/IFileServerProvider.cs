4b467e5be36762ba5d3c51dee0749bfbf53bec91
jrjosecarlos <52382277+jrjosecarlos@users.noreply.github.com>
2019-07-07 22:51:38 -0300

    Renomeados parâmetros para ficarem consistentes com os demais.

--- Services/IFileServerProvider.cs
+++ Services/IFileServerProvider.cs
@@ -14,16 +14,16 @@ namespace WebApiUploadDownload.Services
         /// <summary>
         /// Realiza assincronamente o upload de um arquivo, a partir do conteúdo de uma <see cref="Stream"/>.
         /// </summary>
-        /// <param name="fileName">Nome do arquivo que será criado</param>
+        /// <param name="nomeArquivo">Nome do arquivo que será criado</param>
         /// <param name="stream">Stream com o conteúdo do arquivo para upload. Esta Stream já deve estar aberta.</param>
         /// <returns>Uma <see cref="Task"/> que representa a tarefa do upload a ser feito.</returns>
-        Task UploadFromStreamAsync(string fileName, Stream stream);
+        Task UploadFromStreamAsync(string nomeArquivo, Stream stream);
 
         /// <summary>
         /// Realiza assincronamente o download de um arquivo, que será criado com o nome solicitado.
         /// </summary>
-        /// <param name="fileName">Nome do arquivo que será baixado.</param>
+        /// <param name="nomeArquivo">Nome do arquivo que será baixado.</param>
         /// <returns>Uma <see cref="Task{Stream}"/> que representa a tarefa do download que será feito.</returns>
-        Task<Stream> GetDownloadStreamAsync(string fileName);
+        Task<Stream> GetDownloadStreamAsync(string nomeArquivo);
     }
 }
